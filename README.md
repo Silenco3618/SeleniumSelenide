21a9ce23af3a1e63abc8fb4685a20b3edb09eac2
Julia Silenco <silenko10031986@gmail.com>
2023-01-16 14:17:51 +0400

    add

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/78xfq2ho8ur99ytn?svg=true)](https://ci.appveyor.com/project/Julia33483/seleniumselenide)
---
+++ src/test/java/ru/netology/web/PositivesTest.java
@@ -0,0 +1,64 @@
+package ru.netology.web;
+
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PositivesTest {
+    private WebDriver driver;
+
+    @BeforeAll
+    static void  setUpAll() {
+        System.setProperty("webdriver.chrome.driver", "driver/win/chromedriver.exe");
+    }
+
+    @BeforeEach
+    public void setup() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+    }
+
+    @AfterEach
+    public void tearsDown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    // проверка позитивного сценария
+    public void shouldPositiveTest() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Силенко Юлия");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79272573412");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.cssSelector(".button__text")).click();
+
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String actual = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    // проверка двойной фамилии
+    public void shouldPositiveTest1() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Силенко-Чемкаева Юлия");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79272573412");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.cssSelector(".button__text")).click();
+
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String actual = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
+
+        assertEquals(expected, actual);
+    }
+}
+
